b35e16f58f403dc0a0f94d592ae52d5ba2829f4a
linasaba <linasaba.18@gmail.com>
2023-01-30 19:22:01 +0100

    bdd

---
+++ src/main/java/articles/services/UserService.java
@@ -0,0 +1,32 @@
+package articles.services;
+
+import articles.models.User;
+import articles.repository.UserCrUD;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserService {
+    @Autowired
+    private UserCrUD repository;
+
+    public List<User> findAll() {
+        List<User> users = (List<User>) repository.findAll();
+        return users;
+    }
+
+    public void save(User user) {
+        repository.save(user);
+    }
+
+    public Optional<User> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public void delete(User user) {
+        repository.delete(user);
+    }
+
+}
