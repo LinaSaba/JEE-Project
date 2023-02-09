48106973654fe1fabd1ed227b372445c20c743fa
lfredoc <lea.fredoc@ensg.eu>
2023-02-09 11:27:34 +0100

    security form fonctionnel: encadage des mdp, verif etc.

--- src/main/java/articles/config/ApplicationConfig.java
+++ src/main/java/articles/config/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package articles.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ApplicationConfig extends WebSecurityConfigurerAdapter {
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
--- src/main/java/articles/security/AuthProvider.java
+++ src/main/java/articles/security/AuthProvider.java
@@ -1,62 +1,43 @@
 package articles.security;
 
-import java.util.Optional;
-
-import articles.models.Attempts;
 import articles.models.User;
-import articles.repository.AttemptsRepository;
 import articles.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.LockedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 
-@Component public class AuthProvider implements AuthenticationProvider {
-    private static final int ATTEMPTS_LIMIT = 3;
+
+@Component
+public class AuthProvider implements AuthenticationProvider {
     @Autowired private SecurityUserDetailsService userDetailsService;
     @Autowired private PasswordEncoder passwordEncoder;
-    @Autowired private AttemptsRepository attemptsRepository;
     @Autowired private UserRepository userRepository;
     @Override
-    public Authentication authenticate(Authentication authentication)
-            throws AuthenticationException {
-        String username = authentication.getName();
-        Optional<Attempts>
-                userAttempts = attemptsRepository.findAttemptsByUsername(username);
-        if (userAttempts.isPresent()) {
-            Attempts attempts = userAttempts.get();
-            attempts.setAttempts(0);
-            attemptsRepository.save(attempts);
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        if (authentication.getCredentials() == null) {
+            throw new BadCredentialsException("Password not entered");
         }
-        return authentication;
-    }
-    private void processFailedAttempts(String username, User user) {
-        Optional<Attempts>
-                userAttempts = attemptsRepository.findAttemptsByUsername(username);
-        if (userAttempts.isEmpty()) {
-            Attempts attempts = new Attempts();
-            attempts.setUsername(username);
-            attempts.setAttempts(1);
-            attemptsRepository.save(attempts);
-        } else {
-            Attempts attempts = userAttempts.get();
-            attempts.setAttempts(attempts.getAttempts() + 1);
-            attemptsRepository.save(attempts);
 
-            if (attempts.getAttempts() + 1 >
-                    ATTEMPTS_LIMIT) {
-                user.setAccountNonLocked(false);
-                userRepository.save(user);
-                throw new LockedException("Too many invalid attempts. Account is locked!!");
-            }
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        User user = userRepository.findUserByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Details not found"));
+
+        if (passwordEncoder.matches(password, user.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
+        } else {
+            throw new BadCredentialsException("Password mismatch");
         }
     }
+
     @Override public boolean supports(Class<?> authentication) {
         return true;
     }
